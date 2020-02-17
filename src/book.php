157b4aa30e9593c4f879b74ba201ec2a95a25b3e
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-02-17 15:44:21 -0500

    CRUD functionality

--- src/book.php
+++ src/book.php
@@ -37,12 +37,12 @@ class Book
         return $book;
     }
 
-    //make this a static method 
-    public function findAll()
+    public static function findAll()
     {
+        $book = new static();
         $sql = 'SELECT title, author, pages, category FROM Books ORDER BY title';
-        $statement = $this->database->query($sql);
-        foreach ($this->database->query($sql) as $row) {
+        $statement = $book->database->query($sql);
+        foreach ($statement as $row) {
             print $row['title'] . "\t";
             print $row['author'] . "\t";
             print $row['pages'] . "\t";
@@ -50,39 +50,68 @@ class Book
         }
     }
 
-    //Inserts or updates a record into the database using a save() method on the model
-    //check to see if record exists first, then create new? and if it already exists then update?
-    public function save()
-    {
-    }
 
-    //Deletes the record from the database using a destroy() method on the model
-    public function destroy()
-    {
-    }
-}
-
-var_export(new Book());
-$testBook = new Book();
-$testBook->findAll();
-$testBook->find(1);
-Book::find(2);
-var_export(Book::find(1));
-
-
-
-
-
-
-// Illustrates CRUD functions on a model (all database interaction should happen at the model layer)
-
-// Validates properties on the model using a validate() function which returns true or false as to whether or not the 
-// model is valid 
-
-// The model's validate() function should feed data to an errors() function that returns an array of errors (if any)
-
-// The model's save function should return true or false if a insert or update was successful, and should validate the 
+// The model's save function should return true or false if an insert or update was successful, and should validate the 
 // model prior to attempting to save. If the model is invalid, the save() should not complete and should return false.
 
 // If a model does not save for some reason, the user should be returned to the create or update view and be shown 
 // the errors.
+    public function save($title, $author, $pages, $category, $bookID = null)
+    {
+        if ($bookID == null) {
+            $sql = 'INSERT INTO Books (title, author, pages, category) VALUES (?,?,?,?)';
+            $statement = $this->database->prepare($sql);
+            $statement->execute([$title, $author, $pages, $category]);
+            return;
+        } else {
+            $sql = 'UPDATE Books SET title=?, author=?, pages=?, category=? WHERE bookID = ?';
+            $statement = $this->database->prepare($sql);
+            $statement->bindValue(":title", $title, PDO::PARAM_STR);
+            $statement->bindValue(":author", $author, PDO::PARAM_STR);
+            $statement->bindValue(":pages", $pages, PDO::PARAM_INT);
+            $statement->bindValue(":category", $category, PDO::PARAM_STR);
+            $statement->bindValue(":bookID", $bookID, PDO::PARAM_INT);
+            $statement->execute([$title, $author, $pages, $category, $bookID]);
+        }
+    }
+
+    public function destroy(string $title)
+    {
+        $sql = 'DELETE FROM Books WHERE title = ?';
+        $statement = $this->database->prepare($sql);
+        $statement->execute([$title]);
+    }
+
+    // Validates properties on the model using a validate() function which returns true or false as to 
+    // whether or not the model is valid
+
+    // The model's validate() function should feed data to an errors() function that returns an array 
+    // of errors (if any)
+    public function validate()
+    {
+        $validateBook = new Book();
+        if (true) {
+            return true;
+        } else {
+            $validateBook->errors();
+            return false;
+        }
+    }
+
+    public function errors(): ?array
+    {
+        $errors = [];
+        return $errors;
+    }
+}
+
+
+$testBook = new Book();
+$testBook->findAll();
+// $testBook->find(1);
+// Book::find(2);
+// var_export(Book::find(1));
+// $testBook->destroy('Book Bacon BROKE');
+
+$addBook = new Book();
+$addBook->save('Pride and Prejudice', 'Jane Austen', 750, 'F', 16);
