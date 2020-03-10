93a6970644792c24e3702a70e780ec2508cd69f4
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-03-10 16:01:25 -0400

    Installing Mockery, PHPUnit and Faker

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit2e2cdcd613069305cd400bf14003ab1d
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit2e2cdcd613069305cd400bf14003ab1d::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire2e2cdcd613069305cd400bf14003ab1d($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire2e2cdcd613069305cd400bf14003ab1d($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
