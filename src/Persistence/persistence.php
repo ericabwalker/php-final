affdb47bd7df95dff0f37bc43112b585b7659a6c
Erica Walker <47954578+ericabwalker@users.noreply.github.com>
2020-03-19 12:51:17 -0400

    Adding documentation to persistence interface

--- src/Persistence/persistence.php
+++ src/Persistence/persistence.php
@@ -7,11 +7,29 @@ use Ericabwalker\PHPfinal\Models\Book;
 interface Persistence
 {
 
+    /**
+     * @param int $bookID ID of the book to find.
+     * 
+     * @return Book|null
+     */
     public function find(int $bookID): ?Book;
 
+    /**
+     * Finds all the books in the database, returns null if empty.
+     * 
+     * @return array|null
+     */
     public function findAll(): ?array;
 
+    /**
+     * @param Book $book Book object to save in the database.
+     * 
+     * @return Book|null
+     */
     public function save(Book $book): ?Book;
 
+    /**
+     * @param int $bookID ID of the book to delete. 
+     */
     public function destroy(int $bookID);
 }
